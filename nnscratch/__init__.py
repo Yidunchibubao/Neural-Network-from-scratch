2e27dd8e1775f1d25d367294de247ddad87ee9db
Lyu CHEN <lucaschen2108@gmail.com>
2025-06-17 21:09:30 +0200

    Update __init__.py

--- nnscratch/__init__.py
+++ nnscratch/__init__.py
@@ -1,5 +1,3 @@
-# nnscratch/__init__.py
-
 # -*- coding: utf-8 -*-
 """nnscratch/__init__.py"""
 
@@ -9,7 +7,8 @@ def _get_array_module():
     try:
         import cupy
         try:
-            if cupy.cuda.is_available(): 
+            # 只有在驱动、runtime 全部 OK 时才返回 cupy
+            if cupy.cuda.is_available():
                 return cupy
         except Exception:
             pass
@@ -23,6 +22,7 @@ from .layers      import LinearLayer, ActivationFunction
 from .embeddings  import EmbeddingLayer
 from .network     import NeuralNetwork
 from .utils       import (
+    load_conllu,
     build_vocab,
     one_hot_vector,
     sentence_to_onehot,
@@ -33,7 +33,6 @@ from .utils       import (
 __all__ = [
     "cp", "np",
     "LinearLayer", "ActivationFunction", "EmbeddingLayer", "NeuralNetwork",
-    "build_vocab",
+    "load_conllu", "build_vocab",
     "one_hot_vector", "sentence_to_onehot", "tags_to_onehot", "build_windowed_dataset",
 ]
-
