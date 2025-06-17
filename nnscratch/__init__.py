4b6e630073c03c8b3feb32b917a5e619431eb7a4
Lyu CHEN <lucaschen2108@gmail.com>
2025-06-17 19:21:45 +0200

    Create __init__.py

---
+++ nnscratch/__init__.py
@@ -0,0 +1,27 @@
+# nnscratch/__init__.py
+
+import numpy as np
+try:
+    import cupy as cp
+except ImportError:
+    cp = np
+
+from .layers      import LinearLayer, ActivationFunction
+from .embeddings  import EmbeddingLayer
+from .network     import NeuralNetwork
+from .utils       import (
+    load_conllu,
+    build_vocab,
+    one_hot_vector,
+    sentence_to_onehot,
+    tags_to_onehot,
+    build_windowed_dataset,
+)
+
+
+__all__ = [
+    "cp", "np",
+    "LinearLayer", "ActivationFunction", "EmbeddingLayer", "NeuralNetwork",
+    "load_conllu", "build_vocab",
+    "one_hot_vector", "sentence_to_onehot", "tags_to_onehot", "build_windowed_dataset",
+]
