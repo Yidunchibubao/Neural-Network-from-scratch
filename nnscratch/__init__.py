4e085a0b4c5fca56bc0331fd7f70380c776c57c0
Lyu CHEN <lucaschen2108@gmail.com>
2025-06-17 20:38:32 +0200

    Update __init__.py

--- nnscratch/__init__.py
+++ nnscratch/__init__.py
@@ -1,10 +1,17 @@
 # nnscratch/__init__.py
 
 import numpy as np
-try:
-    import cupy as cp
-except ImportError:
-    cp = np
+
+def _get_array_module():
+    try:
+        import cupy as cp_mod
+        if cp_mod.cuda.runtime.getDeviceCount() > 0:
+            return cp_mod
+    except Exception:
+        pass
+    return np
+
+cp = _get_array_module()
 
 from .layers      import LinearLayer, ActivationFunction
 from .embeddings  import EmbeddingLayer
