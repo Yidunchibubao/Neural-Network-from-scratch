3a67f70706eae74fc280caba7e3d48d20bbf9bf8
Lyu CHEN <lucaschen2108@gmail.com>
2025-06-17 21:04:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     name="nnscratch",
     version="0.1.0",
     description="Neural nets from scratch toolkit for NLP tasks",
-    author="Lyu CHEN, Shuyue GU, Varvara SMIRNOVA",
+    author="Yidunchibubao",
     packages=find_packages(),
     python_requires=">=3.7",
     install_requires=[
-        "numpy>=1.20",
+        "numpy>=1.20", 
     ],
     extras_require={
-        "gpu": ["cupy"],
+        "gpu": ["cupy"], 
     },
     classifiers=[
         "Programming Language :: Python :: 3",
